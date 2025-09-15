e933d6c01a2ce7be40d72cea69e7103657549c4e
Estelle-Prn <peroni.estelle@gmail.com>
2025-09-15 09:17:45 +0200

    corrected the bug of the time zone

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-let romeDate = document.querySelector("#rome .date");
-let romeTime = document.querySelector("#rome .time");
+function upDateTime() {
+  //Rome
+  let romeDate = document.querySelector("#rome .date");
+  let romeTime = document.querySelector("#rome .time");
+  //let romeLocation = moment().tz("Europe / Rome");
 
-romeDate.innerHTML = moment().format("MMM Do YYYY");
-romeTime.innerHTML = moment().format("HH:mm:ss [<span>]A[</span>]");
+  romeDate.innerHTML = moment().tz("Europe/Rome").format("MMM Do YYYY");
+  romeTime.innerHTML = moment()
+    .tz("Europe/Rome")
+    .format("HH:mm:ss [<span>]A[</span>]");
 
-let londonDate = document.querySelector("#london .date");
-let londonTime = document.querySelector("#london .time");
+  //London
+  let londonDate = document.querySelector("#london .date");
+  let londonTime = document.querySelector("#london .time");
+  //let londonLocation = moment().tz("Europe / London");
 
-londonDate.innerHTML = moment().format("MMM Do YYYY");
-londonTime.innerHTML = moment().format("HH:mm:ss [<span>]A[</span>]");
+  londonDate.innerHTML = moment().tz("Europe/London").format("MMM Do YYYY");
+  londonTime.innerHTML = moment()
+    .tz("Europe/London")
+    .format("HH:mm:ss [<span>]A[</span>]");
 
-let tokyoDate = document.querySelector("#tokyo .date");
-let tokyoTime = document.querySelector("#tokyo .time");
+  //Tokyo
+  let tokyoDate = document.querySelector("#tokyo .date");
+  let tokyoTime = document.querySelector("#tokyo .time");
+  let tokyoLocation = moment().tz("Asia/Tokyo");
 
-tokyoDate.innerHTML = moment().format("MMM Do YYYY");
-tokyoTime.innerHTML = moment().format("HH:mm:ss [<span>]A[</span>]");
+  tokyoDate.innerHTML = tokyoLocation.format("MMM Do YYYY");
+  tokyoTime.innerHTML = tokyoLocation.format("HH:mm:ss [<span>]A[</span>]");
 
-let parisDate = document.querySelector("#paris .date");
-let parisTime = document.querySelector("#paris .time");
+  //Paris
+  let parisDate = document.querySelector("#paris .date");
+  let parisTime = document.querySelector("#paris .time");
+  let parisLocation = moment().tz("Europe/Paris");
 
-parisDate.innerHTML = moment().format("MMM Do YYYY");
-parisTime.innerHTML = moment().format("HH:mm:ss [<span>]A[</span>]");
+  parisDate.innerHTML = parisLocation.format("MMM Do YYYY");
+  parisTime.innerHTML = parisLocation.format("HH:mm:ss [<span>]A[</span>]");
+}
+
+upDateTime();
+setInterval(upDateTime, 1000);
